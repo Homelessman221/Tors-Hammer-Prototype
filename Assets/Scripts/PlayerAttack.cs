08538ebd281d16ecc4a77caae600a79334f1bcd9
Homelessman221 <ulrikschaeffer.2002@gmail.com>
2024-03-27 15:15:17 +0100

    feat: set up to make attack stuff later

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -4,5 +4,8 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    
+    public void AttackInput()
+    {
+
+    }
 }
--- Assets/Scripts/PlayerDirections.cs
+++ Assets/Scripts/PlayerDirections.cs
@@ -6,6 +6,8 @@ public class PlayerDirections : MonoBehaviour
 {
     [SerializeField] private Vector2Variable moveVector;
     [SerializeField] private IntVariable isFacingRight;
+    [SerializeField] private IntVariable isFacingUp;
+    [SerializeField] private float moveTreshold;
     private void Update()
     {
         if(moveVector.Value.x < 0)
@@ -15,6 +17,15 @@ public class PlayerDirections : MonoBehaviour
         else if(moveVector.Value.x > 0)
         {
             isFacingRight.Value = 1;
+        }
+
+        if (moveVector.Value.y > moveTreshold)
+        {
+            isFacingUp.Value = 1;
+        }
+        else if (moveVector.Value.y < -moveTreshold)
+        {
+            isFacingUp.Value= 0;
         }
     }
 }
